e7b852a53f597d29178b74f78205953b0fecef77
syounng <stellaj0522@gmail.com>
2025-06-19 07:47:30 +0900

    feat: Add Book PostMapping API

--- src/main/java/com/sy/service/BookService.java
+++ src/main/java/com/sy/service/BookService.java
@@ -7,6 +7,7 @@ import com.sy.domain.Note;
 import com.sy.dto.BookNoteDetailResponse;
 import com.sy.dto.CreateNoteRequest;
 import com.sy.dto.BookResponse;
+import com.sy.dto.CreateBookRequest;
 
 public interface BookService {
     Book findBookById(Long id);
@@ -15,4 +16,5 @@ public interface BookService {
     List<BookResponse> getBookList();
     void createNote(Long bookId, CreateNoteRequest request, String writer);
     void deleteNote(Long bookId, Long noteId);
+    void createBook(CreateBookRequest request);
 } 
